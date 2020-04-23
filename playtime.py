347fa2441d125c80134d795f42fc75a9cf6c3e3d
Nick Clarke <nickolasclarke@gmail.com>
2020-04-23 14:15:30 -0700

    yet more wip

--- playtime.py
+++ playtime.py
@@ -9,36 +9,38 @@ bids = pd.read_csv('data/as_bid_aggregated_data.csv')
 plans = pd.read_csv('data/as_plan.csv')
 energy_prices = pd.read_csv('data/as_bid_aggregated_data.csv')
 price_vol = pd.read_csv('data/as_price_vol.csv')
-generation = pd.read_csv('data/generation.csv')
+#generation = pd.read_csv('data/generation.csv')
 weather = pd.read_csv('data/weather_forecast_ercot.csv')
 
 data = {'bids':bids,
         'plans':plans,
         'energy_prices':energy_prices,
         'price_vol':price_vol,
-        'generation':generation,
+        #'generation':generation,
         'weather':weather,
        }
+#clean up df-specific bits
+#data['generation'].fillna(0)
 #%%
 # process weather seperately, as hour 23 is indexed as hour -1 on the next day.
-def process_weather(df):
-    df = df.drop_duplicates()
-    weather_negative = df[df['hr_beg'] == -1]
-    #change value to 23rd hour
-    weather_negative['hr_beg'] = 23
-    weather_negative['date'] = pd.to_datetime(df['date'])
-    #change date to one day in the past
-    weather_negative['date'] = pd.to_datetime(weather_negative['date'])-pd.Timedelta('1 days')
-    #TODO results in funky dt objects for 23rd hour, no idea why.
-    df[df['hr_beg'] == -1] = weather_negative
-    df['date'] = pd.to_datetime(df.date)
-    df['hr_beg'] = pd.to_datetime(df['hr_beg'],format='%H').dt.time
-    #TODO HAXXXXXY
-    df = df.set_index(df.apply(lambda x: pd.Timestamp.combine(x['date'], x['hr_beg']), axis=1))
-    df = df.drop(columns=['date','hr_beg'])
-    return df
+# def process_weather(df):
+#     #df = df.drop_duplicates()
+#     weather_negative = df[df['hr_beg'] == -1]
+#     #change value to 23rd hour
+#     weather_negative['hr_beg'] = 23
+#     weather_negative['date'] = pd.to_datetime(df['date'])
+#     #change date to one day in the past
+#     weather_negative['date'] = pd.to_datetime(weather_negative['date'])-pd.Timedelta('1 days')
+#     #TODO results in funky dt objects for 23rd hour, no idea why.
+#     df[df['hr_beg'] == -1] = weather_negative
+#     df['date'] = pd.to_datetime(df.date)
+#     df['hr_beg'] = pd.to_datetime(df['hr_beg'],format='%H').dt.time
+#     #TODO HAXXXXXY
+#     df = df.set_index(df.apply(lambda x: pd.Timestamp.combine(x['date'], x['hr_beg']), axis=1))
+#     df = df.drop(columns=['date','hr_beg'])
+#     return df
 
-data['weather'] = process_weather(weather)
+# data['weather'] = process_weather(weather)
 
 # %%
 def create_dt(input_df,date_col,hr_col,tz ='America/Chicago'):
@@ -47,7 +49,7 @@ def create_dt(input_df,date_col,hr_col,tz ='America/Chicago'):
     '''
     #TODO this would allow idempotence, but it in turn doesnt allow modifying the global from data[key]
     #df = input_df.copy()
-    input_df = input_df.drop_duplicates()
+    #input_df = input_df.drop_duplicates()
     if isinstance(input_df.index, pd.DatetimeIndex):
         return input_df
     #TODO raise exceptions for multiple dt string formats in a column
@@ -71,6 +73,7 @@ for key, df in data.items():
     data[key] = create_dt(data[key],'date','hr_beg')
 
 # %%
+#TODO refactor into function
 times = []
 for key, df in data.items():
     start_ts = df.index.sort_values()[0]
@@ -94,15 +97,25 @@ for key, df in data.items():
           )
 
 # %%
-#attempt to reindex each df with the rull date_range
+#attempt to join on full data
 date_range = pd.date_range(min(times), max(times),freq='H',)#tz='America/Chicago')
-for key, df in data.items():
-    data[key] = data[key].asfreq('H').reindex(index=date_range, fill_value='missing')
+joined_df = pd.DataFrame(date_range, columns=["dt"])
+for key in data.keys():
+    joined_df = joined_df.merge(data[key], how='left',left_on='dt',
+                             right_on=data[key].index,left_index=True)
+    joined_df.reset_index(inplace=True,drop=True)
+
+joined_df = joined_df.set_index('dt')
+join_nan = joined_df.describe().loc['count'] < df.shape[0]
+join_nan = joined_df.describe().loc['count'][join_nan].sort_values()
+joined_na = joined_df.isna().any(axis=1)
+
+
 # %%
-#exploratory joins
-joined_df = bids.join(weather, how='inner')
-joined_df = joined_df.asfreq('H')
-joined_df.describe().loc['count']
-#this should be ~35,158 hours if continuous
+sequence = np.array(joined_df.dropna(how='any').index)    
+longest_seq = max(np.split(sequence, np.where(np.diff(sequence) != 1)[0]+1), key=len)
+longest_seq = pd.DatetimeIndex(longest_seq)
+#fails, says out of indexers are out of bounds
+joined_df.iloc[longest_seq]
 
-
+# %%
